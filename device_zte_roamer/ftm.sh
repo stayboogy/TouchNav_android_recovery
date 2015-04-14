466b7707240deb8056f6e460b467b39a88afc39d
M <m@i5.(none)>
2015-04-14 01:22:00 -0500

    update to latest recovery roamer config

--- device_zte_roamer/ftm.sh
+++ device_zte_roamer/ftm.sh
@@ -3,8 +3,4 @@
 
 echo "enable" >> /sys/devices/platform/android_usb/functions/ftm_mode
 
-#touch /tmp/.ignorebootmessage
-#kill $(ps | grep /sbin/adbd)
-#kill $(ps | grep /sbin/recovery)
-
 /sbin/killrecovery.sh
--- device_zte_roamer/roamer.mk
+++ device_zte_roamer/roamer.mk
@@ -8,12 +8,13 @@ endif
 PRODUCT_COPY_FILES += \
         $(LOCAL_KERNEL):kernel \
 	device/zte/roamer/usbconfig:/root/sbin/usbconfig \
+	device/zte/roamer/usbconfig:/recovery/root/sbin/usbconfig \
 	device/zte/roamer/format-system.zip:recovery/root/tmp/format-system.zip \
 	device/zte/roamer/format-data.zip:recovery/root/tmp/format-data.zip \
 	device/zte/roamer/format-cache.zip:recovery/root/tmp/format-cache.zip \
 	device/zte/roamer/ftm.sh:recovery/root/sbin/ftm.sh \
-	device/zte/roamer/icon_zte_ftm.png:recovery/root/res/icon_zte_ftm.png \
-	device/zte/roamer/icon_zte_ftm_729b.png:recovery/root/res/icon_zte_ftm_729b.png
+	device/zte/roamer/icon_zte_ftm.png:recovery/root/res/images/icon_zte_ftm.png \
+	device/zte/roamer/icon_zte_ftm_729b.png:recovery/root/res/images/icon_zte_ftm_729b.png
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := roamer
